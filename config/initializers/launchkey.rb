213af8a5ae30226a49e38720e2957120bebb0d9c
Justin Veirs <justinveirs@gmail.com>
2014-12-23 18:14:47 -0500

    abusing stupidity

--- config/initializers/launchkey.rb
+++ config/initializers/launchkey.rb
@@ -5,9 +5,9 @@
 # referenced through environment variables which is common when using services
 # such as Heroku and tools like Foreman.
 LaunchKey.configure do |config|
-  config.domain     = ENV['cheeedigital']
-  config.app_key    = ENV['2758695622']
-  config.secret_key = ENV['lv1aahen4l22kqwus68k9khpz0537v36']
+  config.domain     = ENV['']
+  config.app_key    = ENV['']
+  config.secret_key = ENV['']
   config.keypair    = File.read Rails.root.join('config/launchkey_keypair.pem')
-  config.passphrase = ENV['n0VZ73WzTJmc9D9tt1rQLiIuUjx8NKXbTyEptJwwFwWcpGCuesT0LyXBqtIgSm3HrjrJR5WjDnJOJ4M7DJ0Q']
+  config.passphrase = ENV['']
 end
