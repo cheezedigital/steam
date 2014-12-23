03edcc5f8edf1294005cb9b135d0451cdb3a83ca
Justin Veirs <justinveirs@gmail.com>
2014-12-23 18:50:36 -0500

    launchkey

--- config/initializers/launchkey.rb
+++ config/initializers/launchkey.rb
@@ -5,9 +5,10 @@
 # referenced through environment variables which is common when using services
 # such as Heroku and tools like Foreman.
 LaunchKey.configure do |config|
-  config.domain     = ENV['']
-  config.app_key    = ENV['']
-  config.secret_key = ENV['']
+  config.domain     = ENV['LAUNCHKEY_DOMAIN']
+  config.app_key    = ENV['LAUNCHKEY_APP_KEY']
+  config.secret_key = ENV['LAUNCHKEY_SECRET_KEY']
   config.keypair    = File.read Rails.root.join('config/launchkey_keypair.pem')
-  config.passphrase = ENV['']
+  config.passphrase = ENV['LAUNCHKEY_KEYPAIR_PASSPHRASE']
 end
+
